d7fe5846a4de236414f573ba6ed799f57f709523
Alireza Rezaee <alireza-rezaee@outlook.com>
2026-08-08 16:37:17 +0330

    chore: add google auth

--- src/EnglishLeitner.WebClient/Program.cs
+++ src/EnglishLeitner.WebClient/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EnglishLeitner.WebClient;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using EnglishLeitner.WebClient.Services;
+using Excubo.Blazor.ScriptInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,7 +12,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddLocalStorageServices();
 
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<JwtAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => 
+    provider.GetRequiredService<JwtAuthenticationStateProvider>());
+
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScriptInjection();
 
 await builder.Build().RunAsync();
---
+++ src/EnglishLeitner.WebClient/Settings/GoogleSettings.cs
@@ -0,0 +1,6 @@
+namespace EnglishLeitner.WebClient.Settings;
+
+public class GoogleSettings
+{
+    public string ClientId { get; set; } = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+}
