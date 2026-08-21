dba652cd41882de2b55151d2337c42d3491a92cf
Alireza Rezaee <alireza-rezaee@outlook.com>
2026-08-21 12:05:41 +0330

    chore: auto download db and manual db reset

---
+++ src/EnglishLeitner.WebClient/Settings/ApplicationSettings.cs
@@ -0,0 +1,6 @@
+namespace EnglishLeitner.WebClient.Settings;
+
+public class ApplicationSettings
+{
+    public string DatabaseUrl { get; set; } = "REPLACE the SQLite database download URL here.";
+}
